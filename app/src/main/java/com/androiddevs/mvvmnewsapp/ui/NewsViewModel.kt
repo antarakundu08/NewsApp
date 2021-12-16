d8574b476f7ca4951944b7f9e30af31820e45f29
antarakundu08 <antarakundu08@gmail.com>
2021-12-16 15:04:12 +0530

    added room database

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
@@ -0,0 +1,5 @@
+package com.androiddevs.mvvmnewsapp.ui
+
+class NewsViewModel {
+
+}
--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -1,7 +1,8 @@
-package com.androiddevs.mvvmnewsapp.ui.util
+package com.androiddevs.mvvmnewsapp.util
 
 class Constants {
     companion object{
         const val API_KEY="8396f6f362354e89869c0941105b7260"
+        const val BASE_URL="https://newsapi.org"
     }
 }
